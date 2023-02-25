bd3729bda565e68584c8864026dac60501b9c2fb
wagner <wagncarv@outlook.com>
2023-02-25 01:19:52 -0300

    11.8. usando pipes

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,31 +8,31 @@ import { Component } from '@angular/core';
 export class LancamentosPesquisaComponent {
   lancamentos = [
     {
-      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '28/02/2023',
+      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2023, 2, 28),
       dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'
     },
     {
-      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/02/2023',
+      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2023, 2, 10),
       dataPagamento: '09/02/2023', valor: 80000, pessoa: 'Atacado Brasil'
     },
     {
-      tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '20/03/2023',
+      tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(2023, 3, 20),
       dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda'
     },
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: '05/02/2023',
+      tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: new Date(2023, 2, 5),
       dataPagamento: '30/05/2023', valor: 800, pessoa: 'Escola Abelha Rainha'
     },
     {
-      tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '18/03/2023',
+      tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: new Date(2023, 3, 18),
       dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza'
     },
     {
-      tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '10/03/2023',
+      tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: new Date(2023, 3, 10),
       dataPagamento: '09/07/2023', valor: 1750, pessoa: 'Casa Nova Imóveis'
     },
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/03/2023',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2023, 3, 13),
       dataPagamento: null, valor: 180, pessoa: 'Academia Top'
     }
   ];
