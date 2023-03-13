916b8ecceb26152e03fd78a70f8134eeaa6c370d
wagner <wagncarv@outlook.com>
2023-03-12 22:46:33 -0300

    14.11.  criando o Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,20 +13,20 @@ import { SelectButtonModule } from 'primeng/selectbutton';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 
-import { NavbarComponent } from './navbar/navbar.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PessoasModule } from './pessoas/pessoas.module';
+import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ButtonModule,
     CalendarModule,
+    CoreModule,
     DropdownModule,
     FormsModule,
     InputMaskModule,
