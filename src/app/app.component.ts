dd2202f77bc23a8562579761bc5adaf25190fec8
wagner <wagncarv@outlook.com>
2023-03-30 15:36:39 -0300

    17.11 - Internacionalização i18n,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-// import { app-lancamentos - pesquisa } from './app.component';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(
+    private config: PrimeNGConfig,
+    private translateService: TranslateService
+  ) { }
+
+  ngOnInit() {
+    this.translateService.setDefaultLang('pt');
+    this.translateService.get('primeng')
+      .subscribe(res => this.config.setTranslation(res));
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { CoreModule } from './core/core.module';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { ToastModule } from 'primeng/toast';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentoService } from './lancamentos/lancamento.service';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+registerLocaleData(localePt);
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -24,12 +34,19 @@ import { LancamentoService } from './lancamentos/lancamento.service';
     LancamentosModule,
     PessoasModule,
     ToastModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [
     ConfirmationService,
     LancamentoService,
     MessageService,
-    { provide: LOCALE_ID, useValue: 'pt-BR' }
+    TranslateService
   ],
   bootstrap: [AppComponent]
 })
