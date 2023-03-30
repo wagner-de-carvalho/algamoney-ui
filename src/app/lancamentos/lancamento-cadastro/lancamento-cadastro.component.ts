c68c60330d4fd94145b7bcf44b358d936f70b2c9
wagner <wagncarv@outlook.com>
2023-03-30 18:32:14 -0300

    17.17. Listando as categorias cadastradas no dropdown

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from 'src/app/categorias/categoria.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 
 @Component({
   selector: 'app-lancamento-cadastro',
   templateUrl: './lancamento-cadastro.component.html',
   styleUrls: ['./lancamento-cadastro.component.css']
 })
-export class LancamentoCadastroComponent {
+export class LancamentoCadastroComponent implements OnInit {
+  categorias = [];
   tipos = [
-    {label: 'Receita', value: 'RECEITA'},
-    {label: 'Despesa', value: 'DESPESA'}
-  ];
-
-  categorias = [
-    {label: 'Alimentação', value: 1},
-    {label: 'Transporte', value: 2}
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
   ];
 
   pessoas = [
-    {label: 'Albert Einstein', value: 1},
-    {label: 'René Descartes', value: 2},
-    {label: 'Alberto Santos Dumont', value: 3}
+    { label: 'Albert Einstein', value: 1 },
+    { label: 'René Descartes', value: 2 },
+    { label: 'Alberto Santos Dumont', value: 3 }
   ];
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private errorHandler: ErrorHandlerService
+  ) {}
+
+  ngOnInit(): void {
+    this.carregarCategorias();
+  }
+
+  carregarCategorias() {
+    return this.categoriaService.listarTodas()
+    .then(categorias => {
+      this.categorias = categorias.map((c: any) => ({ label: c.nome, value: c.codigo }));
+    })
+    .catch(erro => this.errorHandler.handle(erro));
+  }
 }
