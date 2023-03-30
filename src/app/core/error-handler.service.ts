b6dd29ec92815f0d651a0cee86a3e95e5a0bcd03
wagner <wagncarv@outlook.com>
2023-03-30 17:54:39 -0300

    17.14. mensagem de erro de usuário na exclusão de pessoa

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
@@ -13,8 +14,19 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof HttpErrorResponse
+      && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      try {
+        msg = errorResponse.error[0].mensagemUsuario;
+      } catch (e) { }
+
+      console.error('Ocorreu um erro', errorResponse);
+
     } else {
-      msg = 'Erro ao processar serviço remoto. Tente novamente'
+      msg = 'Erro ao processar serviço remoto. Tente novamente.';
+      console.error('Ocorreu um erro', errorResponse);
     }
 
     this.messageService.add({ severity: 'error', detail: msg })
