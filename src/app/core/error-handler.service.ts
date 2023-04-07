f57eee606fdfe9fe2d0ef67a8ac759628b26a83e
wagner <wagncarv@outlook.com>
2023-04-07 19:45:45 -0300

    19.14. E se o Refresh Token expirar?

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,21 +1,30 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { NotAuthenticatedError } from '../seguranca/money-http-interceptor';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ErrorHandlerService {
-
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private router: Router) {}
 
   handle(errorResponse: any) {
     let msg: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
-    } else if (errorResponse instanceof HttpErrorResponse
-      && errorResponse.status >= 400 && errorResponse.status <= 499) {
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      console.log('erro refresh');
+
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
+    } else if (
+      errorResponse instanceof HttpErrorResponse &&
+      errorResponse.status >= 400 &&
+      errorResponse.status <= 499
+    ) {
       msg = 'Ocorreu um erro ao processar a sua solicitação';
 
       if (errorResponse.status === 403) {
@@ -24,15 +33,14 @@ export class ErrorHandlerService {
 
       try {
         msg = errorResponse.error[0].mensagemUsuario;
-      } catch (e) { }
+      } catch (e) {}
 
       console.error('Ocorreu um erro', errorResponse);
-
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.error('Ocorreu um erro', errorResponse);
     }
 
-    this.messageService.add({ severity: 'error', detail: msg })
+    this.messageService.add({ severity: 'error', detail: msg });
   }
 }
--- src/app/seguranca/money-http-interceptor.ts
+++ src/app/seguranca/money-http-interceptor.ts
@@ -11,6 +11,8 @@ import { mergeMap } from 'rxjs/operators';
 
 import { AuthService } from './auth.service';
 
+export class NotAuthenticatedError {}
+
 @Injectable()
 export class MoneyHttpInterceptor implements HttpInterceptor {
   constructor(private auth: AuthService) {}
@@ -25,6 +27,10 @@ export class MoneyHttpInterceptor implements HttpInterceptor {
     ) {
       return from(this.auth.obterNovoAccessToken()).pipe(
         mergeMap(() => {
+          if (this.auth.isAccessTokenInvalido()) {
+            throw new NotAuthenticatedError();
+          }
+
           req = req.clone({
             setHeaders: {
               Authorization: `Bearer ${localStorage.getItem('token')}`,
