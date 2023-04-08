ddbc0cb921f5ba19c323f34dfdb39711d7d70196
wagner <wagncarv@outlook.com>
2023-04-08 15:10:19 -0300

    20.3. Respondendo requisições com Node.js e Express

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoney-api.herokuapp.com',
-  tokenAllowedDomains: [/algamoney-api.herokuapp.com/],
+  apiUrl: 'http://localhost:8080',
+  tokenAllowedDomains: [/localhost:8080/],
   tokenDisallowedRoutes: [/\/oauth\/token/],
 };
